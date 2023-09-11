27e394ccc8867779af9cc8aef727ec157e70394c
Anders Bjerner <abjerner@skybrud.dk>
2023-09-11 13:21:16 +0200

    Added models for the legacy Terratype package

---
+++ src/Limbo.Umbraco.MigrationsClient/Models/Terratype/TerratypeModel.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Json.Newtonsoft;
+using Skybrud.Essentials.Json.Newtonsoft.Extensions;
+
+namespace Limbo.Umbraco.MigrationsClient.Models.Terratype;
+
+/// <summary>
+/// Class representing a legacy Terratype model.
+/// </summary>
+/// <see>
+///     <cref>https://github.com/Joniff/Terratype</cref>
+/// </see>
+public class TerratypeModel : JsonObjectBase {
+
+    public int? Zoom { get; }
+
+    public TerratypePosition Position { get; }
+
+    public string? Lookup { get; set; }
+
+    private TerratypeModel(JObject json) : base(json) {
+        Zoom = json.GetInt32OrNull("zoom");
+        Position = json.GetObject("position", TerratypePosition.Parse)!;
+        Lookup = json.GetString("lookup");
+    }
+
+    public static TerratypeModel Parse(JObject json) {
+        return new TerratypeModel(json);
+    }
+
+}
---
+++ src/Limbo.Umbraco.MigrationsClient/Models/Terratype/TerratypePosition.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Json.Extensions;
+using Skybrud.Essentials.Json.Newtonsoft;
+using Skybrud.Essentials.Strings.Extensions;
+
+namespace Limbo.Umbraco.MigrationsClient.Models.Terratype;
+
+/// <summary>
+/// Class representing a legacy Terratype position.
+/// </summary>
+/// <see>
+///     <cref>https://github.com/Joniff/Terratype</cref>
+/// </see>
+public class TerratypePosition : JsonObjectBase {
+
+    public string Id { get; }
+
+    public string Datum { get; }
+
+    public double Latitude { get; }
+
+    public double Longitude { get; }
+
+    private TerratypePosition(JObject json) : base(json) {
+        Id = json.GetString("id")!;
+        Datum = json.GetString("datum")!;
+        Latitude = Datum.Split(',')[0].ToDouble();
+        Longitude = Datum.Split(',')[1].ToDouble();
+
+    }
+
+    public static TerratypePosition Parse(JObject json) {
+        return new TerratypePosition(json);
+    }
+
+}
