3e0a1541efb3bed45ef406cec3a3ff26fdc9f8d3
Anders Bjerner <abjerner@skybrud.dk>
2023-09-11 13:14:09 +0200

    Added additional properties to the "VideoPickerList" and "VideoPickerItem" models
    Our legacy video picker allowed titles and description for both the list it self and for each item.

--- src/Limbo.Umbraco.MigrationsClient/Models/Skybrud/VideoPicker/VideoPickerItem.cs
+++ src/Limbo.Umbraco.MigrationsClient/Models/Skybrud/VideoPicker/VideoPickerItem.cs
@@ -1,10 +1,15 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Json.Extensions;
+using Skybrud.Essentials.Json.Newtonsoft;
 
 namespace Limbo.Umbraco.MigrationsClient.Models.Skybrud.VideoPicker {
 
-    public class VideoPickerItem {
+    public class VideoPickerItem : JsonObjectBase {
+
+        public string? Title { get; }
+
+        public string? Description { get; }
 
         public string? Url { get; }
 
@@ -14,7 +19,9 @@ namespace Limbo.Umbraco.MigrationsClient.Models.Skybrud.VideoPicker {
 
         public int ThumbnailId { get; }
 
-        public VideoPickerItem(JObject json) {
+        public VideoPickerItem(JObject json) : base(json) {
+            Title = json.GetString("title");
+            Description = json.GetString("description");
             Url = json.GetString("url");
             Type = json.GetString("type");
             Details = json.GetObject("details");
--- src/Limbo.Umbraco.MigrationsClient/Models/Skybrud/VideoPicker/VideoPickerList.cs
+++ src/Limbo.Umbraco.MigrationsClient/Models/Skybrud/VideoPicker/VideoPickerList.cs
@@ -2,17 +2,21 @@
 using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Json.Extensions;
+using Skybrud.Essentials.Json.Newtonsoft;
 
 namespace Limbo.Umbraco.MigrationsClient.Models.Skybrud.VideoPicker {
 
-    public class VideoPickerList {
+    public class VideoPickerList : JsonObjectBase {
 
         public string? Title { get; }
 
+        public string? Description { get; }
+
         public IReadOnlyList<VideoPickerItem> Items { get; }
 
-        public VideoPickerList(JObject json) {
+        public VideoPickerList(JObject json) : base(json) {
             Title = json.GetString("title");
+            Description = json.GetString("description");
             Items = json.GetArrayItems("items", VideoPickerItem.Parse)!;
         }
 
