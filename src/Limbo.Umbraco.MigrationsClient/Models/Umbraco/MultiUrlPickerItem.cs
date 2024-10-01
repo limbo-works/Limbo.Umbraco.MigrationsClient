ac4914a235b8a5876f50e78f752c8adf1eb6fe93
Anders Bjerner <abjerner@skybrud.dk>
2024-10-01 10:02:04 +0200

    Implemented model for the multi URL picker list

---
+++ src/Limbo.Umbraco.MigrationsClient/Models/Umbraco/MultiUrlPickerItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Json.Newtonsoft.Extensions;
+
+namespace Limbo.Umbraco.MigrationsClient.Models.Umbraco;
+
+public class MultiUrlPickerItem {
+
+    public string Name { get; }
+
+    public string? Udi { get; }
+
+    public string? Target { get; }
+
+    public string? Url { get; }
+
+    private MultiUrlPickerItem(JObject json) {
+        Name = json.GetString("name")!;
+        Udi = json.GetString("udi");
+        Target = json.GetString("target");
+        Url = json.GetString("url");
+    }
+
+    [return: NotNullIfNotNull(nameof(json))]
+    public static MultiUrlPickerItem? Parse(JObject? json) {
+        return json is null ? null : new MultiUrlPickerItem(json);
+    }
+
+}
---
+++ src/Limbo.Umbraco.MigrationsClient/Models/Umbraco/MultiUrlPickerList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Limbo.Umbraco.MigrationsClient.Models.Umbraco;
+
+public class MultiUrlPickerList : IReadOnlyList<MultiUrlPickerItem> {
+
+    private readonly IReadOnlyList<MultiUrlPickerItem> _items;
+
+    public int Count => _items.Count;
+
+    public MultiUrlPickerItem this[int index] => _items[index];
+
+    public MultiUrlPickerList(IReadOnlyList<MultiUrlPickerItem> items) {
+        _items = items;
+    }
+
+    public IEnumerator<MultiUrlPickerItem> GetEnumerator() {
+        return _items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+        return GetEnumerator();
+    }
+
+    public static MultiUrlPickerList Parse(JArray array) {
+
+        List<MultiUrlPickerItem> items = [];
+
+        foreach (JToken token in array) {
+
+            if (token is JObject obj) items.Add(MultiUrlPickerItem.Parse(obj));
+
+        }
+
+        return new MultiUrlPickerList(items);
+
+    }
+
+}
