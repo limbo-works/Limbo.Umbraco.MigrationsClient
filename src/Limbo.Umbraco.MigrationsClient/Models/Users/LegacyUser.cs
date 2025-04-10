99575329fab67cdaab969c586fd2abd89b205351
Anders Bjerner <abj@limbo.works>
2025-04-10 09:18:10 +0200

    Introduced models for representing Umbraco users
    The API package doesn't expose the Umbraco users, but if we export the users directly from the database, we can use the new models to represent them in C#. For some of the recent migrations, I've done something similar in the individual solutions, so I might as well add the models here.

---
+++ src/Limbo.Umbraco.MigrationsClient/Models/Users/LegacyUser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Skybrud.Essentials.Guids;
+using Skybrud.Essentials.Time;
+
+namespace Limbo.Umbraco.MigrationsClient.Models.Users;
+
+public class LegacyUser {
+
+    public int Id { get; }
+
+    public Guid Key { get; }
+
+    public string Login { get; }
+
+    public string Email { get; }
+
+    public string Name { get; }
+
+    public string Language { get; }
+
+    public EssentialsTime CreateDate { get; }
+
+    public EssentialsTime UpdateDate { get; }
+
+    public string? Avatar { get; }
+
+    public bool UserDisabled { get; }
+
+    public bool UserNoConsole { get; }
+
+    public LegacyUser(int id, string login, string email, string name, string language, EssentialsTime createDate, EssentialsTime updateDate, string? avatar, bool userDisabled, bool userNoConsole) {
+        Id = id;
+        Key = GuidUtils.ToGuid(id);
+        Login = login;
+        Email = email;
+        Name = name;
+        Language = language;
+        CreateDate = createDate;
+        UpdateDate = updateDate;
+        Avatar = avatar;
+        UserDisabled = userDisabled;
+        UserNoConsole = userNoConsole;
+    }
+
+    public LegacyUser(int id, Guid key, string login, string email, string name, string language, EssentialsTime createDate, EssentialsTime updateDate, string? avatar, bool userDisabled, bool userNoConsole) {
+        Id = id;
+        Key = key;
+        Login = login;
+        Email = email;
+        Name = name;
+        Language = language;
+        CreateDate = createDate;
+        UpdateDate = updateDate;
+        Avatar = avatar;
+        UserDisabled = userDisabled;
+        UserNoConsole = userNoConsole;
+    }
+
+}
---
+++ src/Limbo.Umbraco.MigrationsClient/Models/Users/LegacyUserList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+#pragma warning disable CS0109 // Member does not hide an inherited member; new keyword is not required
+
+namespace Limbo.Umbraco.MigrationsClient.Models.Users;
+
+public class LegacyUserList : ReadOnlyCollection<LegacyUser> {
+
+    private static LegacyUserList? _empty;
+
+    private Dictionary<int, LegacyUser>? _byId;
+    private Dictionary<Guid, LegacyUser>? _byKey;
+    private Dictionary<string, LegacyUser>? _byLogin;
+    private Dictionary<string, LegacyUser>? _byEmail;
+
+    public static new LegacyUserList Empty => _empty ??= [];
+
+    public LegacyUserList() : base([]) { }
+
+    public LegacyUserList(IList<LegacyUser> list) : base(list) { }
+
+    public bool TryGet(int id, [NotNullWhen(true)] out LegacyUser? result) {
+        _byId ??= this.ToDictionary(x => x.Id);
+        return _byId.TryGetValue(id, out result);
+    }
+
+    public bool TryGet(Guid key, [NotNullWhen(true)] out LegacyUser? result) {
+        _byKey ??= this.ToDictionary(x => x.Key);
+        return _byKey.TryGetValue(key, out result);
+    }
+
+    public bool TryGet(string value, [NotNullWhen(true)] out LegacyUser? result) {
+        _byEmail ??= this.ToDictionary(x => x.Email);
+        _byLogin ??= this.ToDictionary(x => x.Login);
+        return _byEmail.TryGetValue(value, out result) || _byLogin.TryGetValue(value, out result);
+    }
+
+}
